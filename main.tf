836d8fe2386f975a483424ff9304b4b8de8155fa
hdrain2001 <hdrain2001@gmail.com>
2026-06-11 14:37:16 -0400

    Add s3 module

--- main.tf
+++ main.tf
@@ -129,7 +129,7 @@ module "ec2_instances" {
 }
 
 module "terraform-aws-s3-bucket-hdrai" {
-  source  = "app.terraform.io/policy-as-code-training-hdr/terraform-aws-s3-bucket-hdrai/AWS"
+  source  = "app.terraform.io/policy-as-code-training-hdr/terraform-aws-s3-bucket-hdrai/aws"
   version = "1.0.0"
   bucket_name = "lab4-3-bucket-hdrai"
 }
